5da991790dfaa9f452526ddd1093c5262c04b552
Sudarshan <sudarshan4309@gmail.com>
2017-08-13 10:43:30 +0530

    initial commit

---
+++ sequence.R
@@ -0,0 +1,69 @@
+seq(from=2,to=4)
+
+
+seq(from=4,to=2)
+
+
+seq(from=-4,to=4)
+
+
+seq(-4,+4)
+
+
+seq(10,16,2)
+
+
+seq(3,-2,-0.5)
+
+
+seq(to=10,length=10)
+
+
+seq(from=10,length=10)
+
+
+seq(from=10,length=10,by=0.1)
+
+
+x<-2
+seq(1,x,x/10)
+
+
+x<-50
+seq(0,x,x/10)
+
+
+abs(seq(-2,2))
+
+
+sqrt(abs(seq(-6,6, by = 3)))
+
+
+seq(to = 100, length = 8)
+
+
+seq(to = -20, length = 10)
+
+
+seq(to = 22, length = 5, by = 6)
+
+
+seq(from = -10, length = 5, by = -0.3)
+
+
+seq(10)
+
+
+seq(1:10)
+
+
+Y <- c(90, 77, 51, 20, 30, 60, 89) 
+X <- seq(along = Y)
+X
+Y[X[2]]
+
+
+Y <- c(900, 717, 251, 280, 302, 670, 897)  
+X <- seq(along = Y)
+Y[X[2]]
+Y[X[6]]
